2645a0ac3c9cd77d2b98ccaf4e225191be042df8
Aditi Sahu <60737472+aditi30-sahu@users.noreply.github.com>
2021-03-08 17:25:33 +0530

    Add files via upload

---
+++ js/bsr.js
@@ -0,0 +1,10 @@
+
+    document.getElementById("rnt").onclick = function () {
+        location.href = "./rentpg.html";
+    };
+    document.getElementById("by").onclick = function () {
+      location.href = "./products-filter.html";
+  };
+  document.getElementById("sl").onclick = function () {
+    location.href = "./products-filter.html";
+};
