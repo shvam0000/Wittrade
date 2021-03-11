8f1c11aaeec2b6382406a59a05f287137f4eac0b
Aditi Sahu <60737472+aditi30-sahu@users.noreply.github.com>
2021-03-11 09:10:34 +0530

    Update bsr.js

--- js/bsr.js
+++ js/bsr.js
@@ -8,3 +8,23 @@
   document.getElementById("sl").onclick = function () {
     location.href = "./products-filter.html";
 };
+//search
+const search = document.getElementById("search");
+
+search.addEventListener("keyup",(e)=>{
+  e.preventDefault();
+  // const searchValue = search.value.toLowerCase().trim();
+
+    location.href = "./products-filter.html";
+    // const storeProducts = document.querySelectorAll(".store-product");
+    // for(i = 0; i< storeProducts.length; i++)
+    // {
+    //   if(storeProducts[i].classList.contains(searchValue)){
+    //     storeProducts[i].style.display="block";
+    //   }else if(searchValue ==""){
+    //     storeProducts[i].style.display="block";
+    //   }else{
+    //     storeProducts[i].style.display="none";
+    //   }
+    // }
+})
