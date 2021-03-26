b76091f194461c8cef9c0a2534cdd19a6111890f
Shivam <shivam@shivam.ib-wrb304n.setup.in>
2021-03-26 10:24:24 +0530

    Backend

---
+++ Backend/api/models/product.js
@@ -0,0 +1,13 @@
+const mongoose =  require('mongoose');
+
+const productSchema = mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    name:{ type: String, required: true},
+    price:{ type: Number, required: true},
+    productImage:{type:String,required :true},
+    title:{type:String,required:true},
+    description:{type:String,required:true}
+});
+
+
+module.exports = mongoose.model('Product', productSchema);
